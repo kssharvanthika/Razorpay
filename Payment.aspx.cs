c23c56603ca40024966f904ae5eed11192a54691
kssharvanthika <kssharvanthika123@gmail.com>
2024-02-26 11:05:10 +0530

    first commit

---
+++ Payment.aspx.cs
@@ -0,0 +1,29 @@
+﻿using Razorpay.Api;
+using System;
+using System.Collections.Generic;
+
+namespace RazorpaySampleApp
+{
+    public partial class Payment : System.Web.UI.Page
+    {
+        public string orderId;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Dictionary<string, object> input = new Dictionary<string, object>();
+            input.Add("amount", 100); // this amount should be same as transaction amount
+            input.Add("currency", "INR");
+            input.Add("receipt", "12121");
+            input.Add("payment_capture", 1);
+
+            string key = "rzp_test_jq6ngtwXE6BeH0";
+            string secret = "FZpFs1GRpkCTRiDcBBLh9lUK";
+
+            RazorpayClient client = new RazorpayClient(key, secret);
+
+            Razorpay.Api.Order order = client.Order.Create(input);
+            orderId = order["id"].ToString();
+
+
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,69 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("RazorpaySampleApp45")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RazorpaySampleApp45")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ab05d951-dc16-4e53-97fd-ede98a7eb808")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+
+//using System.Reflection;
+//using System.Runtime.CompilerServices;
+//using System.Runtime.InteropServices;
+
+//// General Information about an assembly is controlled through the following 
+//// set of attributes. Change these attribute values to modify the information
+//// associated with an assembly.
+//[assembly: AssemblyTitle("RazorpaySampleApp45")]
+//[assembly: AssemblyDescription("Razorpay SDK for .Net")]
+//[assembly: AssemblyConfiguration("")]
+//[assembly: AssemblyCompany("Razorpay Software Private Limited")]
+//[assembly: AssemblyProduct("Razorpay.API")]
+//[assembly: AssemblyCopyright("Copyright ©  2015")]
+//[assembly: AssemblyTrademark("")]
+//[assembly: AssemblyCulture("")]
+
+//// Setting ComVisible to false makes the types in this assembly not visible 
+//// to COM components.  If you need to access a type in this assembly from 
+//// COM, set the ComVisible attribute to true on that type.
+//[assembly: ComVisible(false)]
+
+//// The following GUID is for the ID of the typelib if this project is exposed to COM
+//[assembly: Guid("31c69168-3829-43f1-af33-13f617b4b196")]
+
+//// Version information for an assembly consists of the following four values:
+////
+////      Major Version
+////      Minor Version 
+////      Build Number
+////      Revision
+////
+//[assembly: AssemblyVersion("1.0.0.0")]
+//[assembly: AssemblyFileVersion("1.0.0.0")]
